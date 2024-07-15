5ba234a9be7bd53f4ab89a64f18d2fb9c27bda76
celinl1220 <145512453+celinl1220@users.noreply.github.com>
2024-07-15 21:01:19 +0900

    Added settings feature (user can alter total number of chances, minimum and maximum (range) of answer: - added new settings button in game screen - clicking settings button shows settings screen - settings screen has input fields for chances, minimum, maximum - settings screen has close button (asks for confirmation when closing with updated values for any of the 3 inputs) - settings screen has save and start new game button (starts a new game with the users new settings applied)

--- script.js
+++ script.js
@@ -2,21 +2,34 @@ const startPageWrapper = document.getElementById("start-page-wrapper");
 const gameTitle = document.getElementById("title");
 const startBtn = document.getElementById("start-btn");
 
+const settingsWrapper = document.getElementById("settings-wrapper");
+const settingsCloseBtn = document.getElementById("settings-close-btn");
+const settingsSaveBtn = document.getElementById("settings-save-btn");
+const chancesInput = document.getElementById("chances");
+const minInput = document.getElementById("min");
+const maxInput = document.getElementById("max");
+
 const gameWrapper = document.getElementById("game-wrapper");
+const promptText = document.getElementById("prompt");
 const numberInput = document.getElementById("number-input");
 const checkBtn = document.getElementById("check-btn");
 const hintResult = document.getElementById("hint-result");
-const chancesText = document.getElementById("chances");
+const chancesLeft = document.getElementById("chances-left");
 
 const homeBtn = document.getElementById("home-btn");
 const restartBtn = document.getElementById("restart-btn");
+const settingsBtn = document.getElementById("settings-btn");
 
-let answer = Math.floor(Math.random() * 100 + 1);
-let chances = 10;
+// default values for chances and answer
+let totalChances = 10;
+let chances = totalChances;
+let min = 1;
+let max = 100;
+let answer = Math.floor(Math.random() * (max - min + 1) + min);
 
 const updateStatus = (color, text) => {
 	hintResult.innerHTML = "";
-	chancesText.innerHTML = `You have <strong>${chances}</strong> chances left!`;
+	chancesLeft.innerHTML = `You have <strong>${chances}</strong> chances left!`;
 	setTimeout(() => {
 		hintResult.style.color = color;
 		hintResult.innerHTML = text;
@@ -41,43 +54,72 @@ const updateCheckBtn = (state) => {
 	};
 }
 
-const restartGame = () => {
-	// console.log("restartGame()");
+const showGame = () => {
 	startPageWrapper.style.display = "none";
+	settingsWrapper.style.display = "none";
 	gameWrapper.removeAttribute("style");
-	chances = 10;
-	answer = Math.floor(Math.random() * 100 + 1);
+}
+
+const openSettings = () => {
+	chancesInput.value = totalChances;
+	minInput.value = min;
+	maxInput.value = max;
+	startPageWrapper.style.display = "none";
+	gameWrapper.style.display = "none";
+	settingsWrapper.removeAttribute("style");
+	settingsCloseBtn.onclick = () => {
+		let confirmClose = true;
+		if(parseInt(chancesInput.value) !== totalChances || parseInt(minInput.value) !== min || parseInt(maxInput.value) !== max) {
+			confirmClose = confirm("Close settings without saving?");
+		}
+		if(confirmClose) {
+			showGame();
+		}
+	};
+	settingsSaveBtn.onclick = () => {
+		totalChances = parseInt(chancesInput.value);
+		min = parseInt(minInput.value);
+		max = parseInt(maxInput.value);
+		if(totalChances && min && max) {
+			restartGame();			
+		} else {
+			alert("Please enter a value for all fields!");
+		}
+	};
+}
+
+const restartGame = () => {
+	showGame();
+	chances = totalChances;
+	answer = Math.floor(Math.random() * (max - min + 1) + min);
+	promptText.textContent = `Guess a number between ${min} and ${max}`;
 	numberInput.value = "";
 	numberInput.disabled = false;
 	numberInput.focus();
-	// checkBtn.textContent = "Check"
 	updateStatus("white", "");
 	updateCheckBtn(buttonFunctions[0]);
 
-	// console.log("answer:", answer);
-
 	homeBtn.addEventListener("click", titlePage);
 	restartBtn.addEventListener("click", restartGame);
+	settingsBtn.addEventListener("click", openSettings);
 }
 
 const endGame = () => {
-	// console.log("endgame()");
 	numberInput.disabled = true;
 	updateCheckBtn(buttonFunctions[1]);
-	// checkBtn.textContent = "Restart";
-	// checkBtn.addEventListener("click", restartGame);
 }
 
 const checkInputValid = () => {
 	const guess = parseInt(numberInput.value);
 	// console.log("guessed:", guess);
+	// console.log("min:", min);
+	// console.log("max:", max);
 	numberInput.value = "";
-	// chancesText.textContent = `ans: ${answer}, guess: ${guess}`;
 	if (guess === answer) {
 		chances--;
 		updateStatus("green", "Congratulations!");
 		endGame();
-	} else if (guess < answer && guess > 0) {
+	} else if (guess < answer && guess >= min) {
 		chances--;
 		if (chances === 0) {
 			updateStatus("red", "You have <strong>lost</strong>...");
@@ -85,7 +127,7 @@ const checkInputValid = () => {
 		} else {
 			updateStatus("white", "Your guess is <strong>low</strong>");
 		}
-	} else if (guess > answer && guess < 101) {
+	} else if (guess > answer && guess <= max) {
 		chances--;
 		if (chances === 0) {
 			updateStatus("red", "You have <strong>lost</strong>..");
@@ -100,6 +142,7 @@ const checkInputValid = () => {
 
 const titlePage = () => {
 	gameWrapper.style.display = "none";
+	settingsWrapper.style.display = "none";
 	startPageWrapper.removeAttribute("style");
 	startBtn.addEventListener("click", restartGame);
 }
